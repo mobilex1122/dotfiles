89c0f05113231f34ac076fd7de911718b3c1ed49
mobilex1122 <60009551+mobilex1122@users.noreply.github.com>
2023-06-08 18:13:54 +0200

    Added micro configuration

--- setup-symlink.sh
+++ setup-symlink.sh
@@ -1,4 +1,10 @@
 # /bin/bash
 
+## Kitty terminal
 ln -s ~/.dotfiles/kitty/ ~/.config/
+
+## Git
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
+## micro editor
+ln -s ~/.dotfiles/micro/settings.json .config/micro/settings.json
