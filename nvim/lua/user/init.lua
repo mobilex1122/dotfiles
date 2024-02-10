b5e0c1676fca2e742c9d1427a7eb1e06ce60562f
Jan Palma <honzik@janpalma.cz>
2024-02-10 14:23:25 +0100

    Created lime LSP custom configurtion for Haxe lime projects
    For now supports only HashLink debug exports
    Run `lime test hl` at least once after dependency change to build haxe config files

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,3 +1,25 @@
 return {
   colorscheme = "monokai-pro",
+  lsp = {
+    servers = {
+      "lime_lsp",
+    },
+    config = {
+      lime_lsp = function()
+        return {
+          cmd = {'haxe-language-server'},
+          filetypes = {"haxe"},
+          settings = {
+            haxe = {
+              executable = "haxe"
+            }
+          },
+          init_options = {
+            displayArguments = {"export/hl/haxe/debug.hxml"},
+          },
+          root_dir = require("lspconfig.util").root_pattern("export/hl/haxe/debug.hxml");
+        }
+      end,
+    }
+  }
 }
