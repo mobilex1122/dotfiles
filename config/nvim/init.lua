43a4a9dd819b6d36fb8ccf86ccda7e1571e23b11
Jan Palma <honzik@janpalma.cz>
2024-09-24 17:07:03 +0200

    Nvim update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,3 +2,17 @@ require("config.lazy")
 
 -- Set colorscheme after options
 vim.cmd('colorscheme flexoki-dark')
+vim.opt.termguicolors = true
+vim.notify = require("notify")
+
+vim.opt.fillchars = { eob = " " }
+
+local wk = require("which-key") 
+
+wk.add({
+	{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Tree View", mode = 'n' }, -- File explorer
+	{ "<leader>l", group = "Lazy Plugin Manager"},
+	{ "<leader>li", "<cmd>Lazy install<cr>", desc = "Install"}
+})
+
+
