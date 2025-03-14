8800de43ef6ff86a4ffa3747a053e9b9f1b912cf
Jan Palma <honzik@janpalma.cz>
2025-03-14 10:07:17 +0100

    New Powerline

--- term/powershell/global.ps1
+++ term/powershell/global.ps1
@@ -1,6 +1,6 @@
 ## Global config (Used on all systems) ##
 
-oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "$PSScriptRoot/../omp/config.json" | Invoke-Expression
 
 ## Personal Preference
 Set-Alias -Name code -Value codium
