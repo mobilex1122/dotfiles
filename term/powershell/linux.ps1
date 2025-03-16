6042afef40cf08c1fa1fff13a13a83adb8203a2f
mobilex <honzik@janpalma.cz>
2025-03-16 15:24:19 +0100

    Added FullSystemUpdate command

--- term/powershell/linux.ps1
+++ term/powershell/linux.ps1
@@ -1,3 +1,16 @@
 if ($IsIDE -ne $True) {
     fastfetch
 }
+
+
+# Arch Linux Only
+function FullSystemUpdate() {
+    Write-Output "Updating pacman..."
+    sudo pacman -Syyu
+
+    Write-Output "Updating yay..."
+    yay -Syu
+
+    Write-Output "Updating flatpak..."
+    flatpak update
+}
