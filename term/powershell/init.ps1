063c250fc3c923b2b6574a16afb1a68a20680ee8
Jan Palma <honzik@janpalma.cz>
2025-04-07 15:38:48 +0200

    Added log and enforced $HOME
    Enforces $HOME only when first opened in System32

--- term/powershell/init.ps1
+++ term/powershell/init.ps1
@@ -2,7 +2,32 @@
 
 
 ## Allows showing simple version of the shell if used in terminal.
+Clear-Host
+
 $Global:IsIDE = $env:IsIDE -eq "1"
+
+$Global:StartLog = @()
+function Start-Log-Log() {
+    param (
+        $msg
+    )
+
+    $Global:StartLog += $msg
+}
+
+
+function Start-Log-Print() {
+    Write-Output "PWSH Startup log"
+    Write-Output "================"
+    foreach ($item in $Global:StartLog) {
+        Write-Host $item
+    }
+
+}
+
+
+Set-Alias -Name slog -Value Start-Log-Log
+Set-Alias -Name start-log -Value Start-Log-Print
 
 if ($IsIDE) {
     . "$PSScriptRoot/ide-mode.ps1"    
--- term/powershell/windows.ps1
+++ term/powershell/windows.ps1
@@ -1,3 +1,12 @@
 if ($IsIDE -ne $True) {
     fastfetch
 }
+
+
+
+
+
+if ($PWD.Path  -eq "C:\Windows\System32") {
+    slog "Forcefully set Home location!"
+    Set-Location $HOME
+}
